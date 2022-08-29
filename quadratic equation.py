156861d4b4e4d5c108bc1cb818a0ab3da5430252
Pramodreddy0707 <112369481+Pramodreddy0707@users.noreply.github.com>
2022-08-29 14:19:11 +0530

    Add files via upload

---
+++ quadratic equation.py
@@ -0,0 +1,10 @@
+import cmath
+a = int(input("enter the number:"))
+b = int(input("enter the number:"))
+c = int(input("enter the number:"))
+d = (b**2) - (4*a*c)
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
+print("teh roots are")
+print(sol1)
+print(sol2)
